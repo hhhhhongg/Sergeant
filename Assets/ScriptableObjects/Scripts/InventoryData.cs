ebeeea15c41ed04e46eff3f5004a62d529ca59e7
Chhhhong <101609335+Chhhhong@users.noreply.github.com>
2023-12-18 20:22:28 +0900

    Add Scene, Inventory UI
    Add HongScene
    Add Inventory UI

---
+++ Assets/ScriptableObjects/Scripts/InventoryData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "myItems", menuName = "TopDownController/InventoryData", order = 2)]
+public class InventoryData : ScriptableObject
+{
+    public ItemStats[] myItems;
+}
